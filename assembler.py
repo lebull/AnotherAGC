929501d196bab3f54fddac1a28ea19608444fed0
ADS user <ADSUSER@Stephens-MacBook-Pro.local>
2018-05-07 13:45:47 -0500

    Fixin up assembler

--- assembler.py
+++ assembler.py
@@ -3,12 +3,22 @@
 import re
 
 if __name__ == "__main__":
-    filePath = "/Users/administrator/Projects/AnotherAGC/Apollo-11/Comanche055/GIMBAL_LOCK_AVOIDANCE.agc"
+    filePath = "Apollo-11/Comanche055/GIMBAL_LOCK_AVOIDANCE.agc"
 
     with open(filePath) as testFile:
         lines = testFile.read().split("\n")
+
+    lineNumber = 0
+
     for line in lines:
-        #line = re.sub(r'(\t)*', "\t", line)
-        words = line.split("\t")
-        print "----\n{}".format(words)
+        line = re.sub(r'(\s|(\\t))+', "\t", line)
+        line = re.sub(r'#(.)*', "", line)
+        line = re.sub(r'\A(\s)*\Z', "", line)
+
+
+        if line != "":
+            #print line
+            words = line.split("\t")
+            print "{}: {}".format(lineNumber, words)
+            lineNumber+=1
     #Split stuff
