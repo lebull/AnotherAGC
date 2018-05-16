5a794751f47b0287a409e3c98507662c9b975918
ADS user <ADSUSER@Stephens-MacBook-Pro.local>
2018-05-16 12:54:25 -0500

    Starting up the psudo program

--- Rainbow/main.py
+++ Rainbow/main.py
@@ -3,17 +3,97 @@ import sys
 import time
 import random
 
-from screen import Screen
+from screen import Screen, ScreenColors
 
 screen = Screen((10, 20))
 
-for t in range(10000):
-    pos = (random.randint(0, tetris.size[0]-1), random.randint(0, tetris.size[1]-1))
-    screen.data[pos[1]][pos[0]] = ScreenColors.random()
-screen.printout()
+LINE = 1                #def
+NUM_LINE = 4            #def
+LINE_SHAPES = [
+#   |.   .   .   .   |
+    "    XXXX        ",
+    "  X   X   X   X ",
+    "        XXXX    ",
+    " X   X   X   X  "
+]
 
-    #time.sleep(0.01)
+SQUARE = "square"       #def
+NUM_SQUARE = 1          #def
+SQUARE_SHAPES = [
+    "     XX  XX     "
+]
 
-#sys.stdout.write("\033[41m  \033[0m\n")
-raw_input("Enter to continue.")
-screen.clear()
+ELL = "ell"             #def
+
+blocks = {
+    "ell": [],
+    "bell": [],
+    "tee": [],
+    "zig": [],
+    "zag": []
+}
+
+def blitBlock(shape, orientation, pos, unblit):
+
+    #case
+    if(shape == LINE):
+        shapeData = LINE_SHAPES[orientation]
+
+    i = 0
+    while i < 16:
+        targetPos = (pos[0] + i/4, pos[1] + i%4)
+        if(shapeData[i] == "X"):
+            if(unblit):
+                color = ScreenColors.BG_DEFAULT
+            else:
+                color = ScreenColors.BG_RED
+            #should be handled by its own opcode
+            setScreenColor((targetPos[1], targetPos[0]), color)
+        i+=1
+
+def setScreenColor(pos, color):
+    screen.data[pos[0]][pos[1]] = color
+
+#16 bit var
+currentShape = {
+    "shape": LINE, #3
+    "orientation": 0,     #2
+    "pos": (0, 0)         #5, 6
+}
+
+#Rotates the currentShape if allowed.  Does nothing otherwise.
+def rotate(direction):
+    global currentShape
+    if(currentShape["shape"] == LINE):
+        pass
+
+#Return a 4x4 grid of occupied spots on the board, encoded
+# as 16 bits.  OOBs will be considered "occupied".
+def getQuartFromBoard(pos):
+    pass
+
+#Returns true if any two corrisponding spots of two quarts are both occupied.
+#At lower levels, this should be the same as a series of ANDS
+def doesConflict(quart1, quart2):
+    pass
+
+
+#Main execution
+for i in range(8):
+    currentShape["orientation"] = i % NUM_LINE
+    currentShape["pos"] = (currentShape["pos"][0], currentShape["pos"][1] + 1)
+
+    blitBlock(
+        currentShape["shape"],
+        currentShape["orientation"],
+        currentShape["pos"],
+        False)
+    screen.printout()
+
+    time.sleep(0.5)
+
+    blitBlock(
+        currentShape["shape"],
+        currentShape["orientation"],
+        currentShape["pos"],
+        True)
